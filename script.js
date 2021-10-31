0d1a24c893592aacfa9923e955c5a877de6208de
JerryLeandro <jerryleandro41@gmail.com>
2021-10-31 17:03:21 -0300

    js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Desenvolvedor", "Full Stack", "Java"],
+        strings: ["Desenvolvedora", "Full Stack", "PHP"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Desenvolvedor", "Full Stack", "Java"],
+        strings: ["Desenvolvedora", "Full Stack", "PHP"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
